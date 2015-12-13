5609d212361b3a4aa73ccfce109972913c080ffa
pascalheraud <pascal.heraud@laroueverte.com>
2015-12-14 00:45:40 +0100

    Added license info

--- src/main/java/org/stjs/bridge/cloudinary/CloudinaryResult.java
+++ src/main/java/org/stjs/bridge/cloudinary/CloudinaryResult.java
@@ -1,3 +1,21 @@
+/*
+    ST-JS-Cloudinary, a ST-JS bridge for cloudinary_js
+    (c) 2015 La Roue Verte
+     
+	This file is part of ST-JS-Cloudinary.
+
+    ST-JS-Cloudinary is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or any later version.
+
+    ST-JS-Cloudinary is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with ST-JS-Cloudinary.  If not, see <http://www.gnu.org/licenses/>
+ */
 package org.stjs.bridge.cloudinary;
 
 import org.stjs.javascript.annotation.SyntheticType;
@@ -18,5 +36,5 @@ public class CloudinaryResult {
 	public String url;
 	public String secure_url;
 	public String path;
-	
+
 }
